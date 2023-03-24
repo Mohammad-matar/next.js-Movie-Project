73c72f94f42e832c5ef9ff553c1d8cde72969566
Matarov <moe.matar1998@gmail.com>
2023-03-24 13:59:06 +0200

    get all the data

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
+export async function generateStaticParams() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+
+  return res.results.map((movie) => ({
+    movie: toString(movie.id),
+  }));
+}
+
 export default async function MovieDetails({ params }) {
   const { movie } = params;
   const imagePath = "https://image.tmdb.org/t/p/original";
@@ -16,6 +29,15 @@ export default async function MovieDetails({ params }) {
         <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-lg text-sm">
           {res.status}
         </h2>
+        <Image
+          className="my-12 w-full"
+          src={imagePath + res.backdrop_path}
+          alt={res.title}
+          width={1000}
+          height={1000}
+          property
+        />
+        <p>{res.overview}</p>
       </div>
     </div>
   );
